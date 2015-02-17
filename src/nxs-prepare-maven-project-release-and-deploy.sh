c268b13cdc6692dc3873ad3442608b7c83cbb97d
fnikitin <frederic.nikitin@gmail.com>
2015-02-17 12:28:28 +0100

    add version number

--- src/nxs-prepare-maven-project-release-and-deploy.sh
+++ src/nxs-prepare-maven-project-release-and-deploy.sh
@@ -6,13 +6,16 @@ set -o errexit  # make your script exit when a command fails.
 set -o pipefail # prevents errors in a pipeline from being masked. If any command in a pipeline fails, that return code will be used as the return code of the whole pipeline.
 set -o nounset  # exit when your script tries to use undeclared variables.
 
+VERSION=0.1.0
+
 function echoUsage() {
     echo "This script prepares and deploys a new maven project release on nexus."
-    echo "usage: $0 [-hmn] <repo>"
+    echo "usage: $0 [-hmnv] <repo>"
     echo "Params:"
     echo " <repo> maven project git repository"
     echo "Options:"
     echo " -h print usage"
+    echo " -v print version"
     echo " -m pre: merge branch develop to master"
     echo " -n post: prepare next snapshot version in branch develop"
 }
@@ -20,10 +23,13 @@ function echoUsage() {
 MERGE_DEVELOP_TO_MASTER=
 BUILD_NEXT_SNAPSHOT=
 
-while getopts 'hmn' OPTION
+while getopts 'hmnv' OPTION
 do
     case ${OPTION} in
     h) echoUsage
+        exit 0
+        ;;
+    v) echo v${VERSION}
         exit 0
         ;;
     m) MERGE_DEVELOP_TO_MASTER=1
@@ -111,7 +117,7 @@ if [ ${MERGE_DEVELOP_TO_MASTER} ]; then
 fi
 
 # build
-mvn clean install
+mvn clean install #-DskipTests
 
 # get release version to prepare
 getNextReleaseVersion
@@ -129,7 +135,6 @@ git tag -a v${RELEASE_VERSION} -m "tag v${RELEASE_VERSION}"
 git push origin master --tags
 
 # deploy on nexus
-
 mvn deploy
 
 # change branch to develop
