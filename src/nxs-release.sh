57e699f6fe5aa4affbfa37e421e3faf2e634aa0e
fnikitin <frederic.nikitin@gmail.com>
2015-09-28 18:49:17 +0200

    Adding prompt to confirm execution as jenkins

--- src/nxs-release.sh
+++ src/nxs-release.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # This script prepares and publish a maven artefact to nexus.
+# Execute the following command in a jenkins job: yes|nxs-release.sh <repo>
 
 set -o errexit  # make your script exit when a command fails.
 set -o pipefail # prevents errors in a pipeline from being masked. If any command in a pipeline fails, that return code will be used as the return code of the whole pipeline.
 set -o nounset  # exit when your script tries to use undeclared variables.
 
 function echoUsage() {
-    echo "This script prepares and deploys a new maven project release on nexus."
+    echo "ONLY JENKINS SHOULD EXECUTE THIS SCRIPT. It prepares and deploys a new release on nexus repository."
     echo "usage: $0 [-h] <repo>"
     echo "Params:"
     echo " <repo> maven project git repository"
@@ -55,30 +56,50 @@ getNextReleaseVersion () {
 
     if [[ ! ${devVersion} =~ [0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT ]]; then
 
-        echo "cannot release ${RELEASE_NAME} v${devVersion}: not a snapshot (develop) version"
+        echo "failed (cannot release ${RELEASE_NAME} v${devVersion}: snapshot (develop) version expected)"
         exit 3
     fi
 
     RELEASE_VERSION=${devVersion%-*}
 }
 
-echo "move to ${GIT_REPO}"
+read -p "The following script should be executed by Jenkins - Are you Jenkins?[y/N] " -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Only Jenkins should execute this script!"
+    exit 1
+fi
+
+echo "changing directory to ${GIT_REPO}"
 cd ${GIT_REPO}
 
+echo -n "testing git branch... "
+currentBranch=$(git rev-parse --abbrev-ref HEAD)
+if [ ! ${currentBranch} = "master" ]; then
+    echo "failed (cannot deploy to production nexus repository: master branch expected)"
+    exit 2
+fi
+echo "done"
+
 # get release version to prepare
+echo -n "getting next release... "
 getNextReleaseVersion
+echo "done (${RELEASE_VERSION}")
 
 # prepare new version
-echo preparing ${RELEASE_NAME} v${RELEASE_VERSION}...
+echo "preparing ${RELEASE_NAME} v${RELEASE_VERSION}... "
 mvn versions:set -DnewVersion=${RELEASE_VERSION} -DgenerateBackupPoms=false
 
 ###### Clean, test and deploy on nexus
+echo "deploying on nexus release repository... "
 mvn clean deploy
 
 ###### Add, commit and push to master
+echo "adding and committing in git... "
 git add -A
 git commit -m "New release version ${RELEASE_VERSION}"
 
+echo "tagging and pushing to origin master... "
 # create a new release tag
 git tag -a v${RELEASE_VERSION} -m "tag v${RELEASE_VERSION}"
 git push origin master --tags
