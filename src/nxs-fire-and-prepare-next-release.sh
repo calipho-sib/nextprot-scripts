185fd3e002c2f5b834d946627aa3117b2950b3fb
fnikitin <frederic.nikitin@gmail.com>
2015-09-30 17:04:14 +0200

    Accept version number X.Y.Z and remove 'no-fast-forward' merge option

--- src/nxs-fire-and-prepare-next-release.sh
+++ src/nxs-fire-and-prepare-next-release.sh
@@ -2,7 +2,7 @@
 
 # This script fires indirectly a new production release (through jenkins) and prepares next development release
 
-# ex1: bash -x nxs-fire-and-prepare-next-release.sh 0.2.1
+# ex1: bash -x nxs-fire-and-prepare-next-release.sh 0.2.0
 
 set -o errexit  # make your script exit when a command fails.
 set -o pipefail # prevents errors in a pipeline from being masked. If any command in a pipeline fails, that return code will be used as the return code of the whole pipeline.
@@ -12,7 +12,7 @@ function echoUsage() {
     echo "usage: $0 <next-snapshot-version> [repo]" >&2
     echo "This script fires indirectly a new production release (through jenkins) and prepares next development release with the given version (-SNAPSHOT is added automatically)"
     echo "Params:"
-    echo " <next-snapshot-version> next snapshot version"
+    echo " <next-snapshot-version> next snapshot version (MAJOR.MINOR.PATCH)"
     echo " <repo> optional maven project git repository"
     echo "Options:"
     echo " -h print usage"
@@ -35,22 +35,27 @@ shift $(($OPTIND - 1))
 GIT_REPO="./"
 VERSION=
 
-if [ $# -lt 1 ]; then
-    echo "missing arguments: Specify the new version (example 0.2.1)"  >&2
-    echoUsage; exit 1
-else
-    VERSION=$1
-    if [ $# -eq 2 ]; then
-        GIT_REPO=$2
-    fi
-fi
-
 checkSnapshotVersion () {
 
     if [[ ! $1 =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
 
-        echo "FAILS: invalid version format (expect format:'X.Y.Z')"
+        echo "FAILS: invalid version format (expect version format:'MAJOR.MINOR.PATCH')"
         exit 2
+    fi
+}
+
+checkSnapshotMajorMinorVersion () {
+
+    if [[ ! $1 =~ [0-9]+\.[0-9]+ ]]; then
+
+        echo "FAILS: invalid version format (expect version format:'MAJOR.MINOR')"
+        exit 2
+    fi
+
+    if [[ $1 =~ ([0-9]+\.[0-9]+)\.([0-9]+) ]]; then
+        VERSION=${BASH_REMATCH[1]}.0
+        echo "WARN: patch number '${BASH_REMATCH[2]}' in '$1' will be set to 0"
+        echo -n "Reset version to ${VERSION} "
     fi
 }
 
@@ -69,7 +74,7 @@ askUserForConfirmation () {
     read -p "$1[y/N] " -r
     if [[ ! $REPLY =~ ^[Yy]$ ]]
     then
-        echo Exit script on user request
+        echo "-- Exit script on user request"
         exit 4
     fi
 }
@@ -79,31 +84,41 @@ checkGitRepo () {
     gitStatus=$(git status -s)
 
     if [[ ${gitStatus} == "" ]]; then
-        echo "Clear git repository!";
+        echo "-- git repository in branch $(git rev-parse --abbrev-ref HEAD) is clean";
     else
-        echo "Git working directory should be clean!"
+        echo "git working directory should be clean!"
 
         git status
 
         if [[ ! ${gitStatus} =~ " ??**" ]]; then
             askUserForConfirmation "There are some untracked files in branch $(git rev-parse --abbrev-ref HEAD) - Do you want to proceed for releasing ?"
         else
-            echo "Cannot make release"
+            echo "FAILS: cannot make release"
             exit 5
         fi
     fi
 }
 
-echo -n "checking snapshot version argument (${VERSION})... "
-checkSnapshotVersion ${VERSION}
-echo "OK"
+if [ $# -lt 1 ]; then
+    echo "missing version number 'MAJOR.MINOR' (example 0.2)"  >&2
+    echoUsage; exit 6
+else
+    VERSION=$1
+    echo -n "-- checking snapshot version argument (${VERSION})... "
+    checkSnapshotVersion ${VERSION}
+    echo "OK"
 
-echo -n "changing to git repository directory '${GIT_REPO}'... "
+    if [ $# -eq 2 ]; then
+        GIT_REPO=$2
+    fi
+fi
+
+echo -n "-- changing to git repository directory '${GIT_REPO}'... "
 cd ${GIT_REPO}
 echo "OK"
 
 # check it is a maven project
-echo -n "checking maven project... "
+echo -n "-- checking maven project... "
 checkMavenProject
 echo "OK"
 
@@ -119,7 +134,7 @@ git pull origin master
 
 checkGitRepo
 
-git merge -X theirs develop --no-commit --no-ff
+git merge -X theirs develop --no-commit
 git status
 askUserForConfirmation "Do you want to commit?"
 git commit -m "Merging develop to master for next release"
@@ -127,13 +142,11 @@ git commit -m "Merging develop to master for next release"
 checkGitRepo
 
 git push origin master
-echo "Jenkins will fire at this point or in 5mins (but we don't wait for it to finish) we assume everything is fine :) "
+echo "-- Jenkins will fire at this point or in 5mins (but we don't wait for it to finish) we assume everything is fine :) "
 
 ################# DEVELOP BRANCH
 git checkout develop
 mvn versions:set -DnewVersion=${VERSION}-SNAPSHOT -DgenerateBackupPoms=false
 git add -A
 git commit -m "Preparing next development snapshot version v${VERSION}"
-
-checkGitRepo
 git push origin develop
