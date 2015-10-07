b7082389642e6e4e208af33ab9ebcc72f38abff4
fnikitin <frederic.nikitin@gmail.com>
2015-10-07 16:16:53 +0200

    Removing -x from bash call

--- test/scenarii/nxs-fire-and-prepare-next-release/scenario1_snapshot-version-should-be-valid.sh
+++ test/scenarii/nxs-fire-and-prepare-next-release/scenario1_snapshot-version-should-be-valid.sh
@@ -3,8 +3,9 @@
 cd ${REPO_WO_DEP}
 git checkout develop
 
-bash -x ${NX_SCRIPTS}/src/nxs-fire-and-prepare-next-release.sh koko
+bash ${NX_SCRIPTS}/src/nxs-fire-and-prepare-next-release.sh koko
 if [ $? != 2 ]; then
     echo "Assertion failed" >&2
     exit 3
 fi
+echo "TEST PASSED"
