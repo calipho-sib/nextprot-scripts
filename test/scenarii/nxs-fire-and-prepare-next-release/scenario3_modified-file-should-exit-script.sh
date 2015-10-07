5aa8b7c751978b0e065f2a2a8c0c70804eeea6e5
fnikitin <frederic.nikitin@gmail.com>
2015-10-07 17:03:37 +0200

    Add scenario 3 to test

--- test/scenarii/nxs-fire-and-prepare-next-release/scenario3_modified-file-should-exit-script.sh
+++ test/scenarii/nxs-fire-and-prepare-next-release/scenario3_modified-file-should-exit-script.sh
@@ -3,10 +3,9 @@
 cd ${REPO_WO_DEP}
 git checkout develop
 
-currentDevVersion=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)'`
-bash ${NX_SCRIPTS}/src/nxs-fire-and-prepare-next-release.sh ${currentDevVersion%-SNAPSHOT}
-if [ $? != 13 ]; then
+echo -e 'Yo' >> README.md
+bash ${NX_SCRIPTS}/src/nxs-fire-and-prepare-next-release.sh 0.10.0
+if [ $? != 5 ]; then
     echo "Assertion failed" >&2
     exit 4
 fi
-echo "TEST PASSED"
