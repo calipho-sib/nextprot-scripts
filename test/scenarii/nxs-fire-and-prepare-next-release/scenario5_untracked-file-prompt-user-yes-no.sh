71bb91e82c8b83fd7b61ba5c17c5bd3020620aa5
fnikitin <frederic.nikitin@gmail.com>
2015-10-07 18:10:05 +0200

    Add scenario 5 to test

---
+++ test/scenarii/nxs-fire-and-prepare-next-release/scenario5_untracked-file-prompt-user-yes-no.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd ${REPO_WO_DEP}
+git checkout develop
+
+touch yo
+printf 'y\nn' | bash ${NX_SCRIPTS}/src/nxs-fire-and-prepare-next-release.sh ${NEXT_REPO_WO_DEP_VERSION_DEVELOP}
+if [ $? != 4 ]; then
+    echo "Assertion failed" >&2
+    exit 15
+fi
