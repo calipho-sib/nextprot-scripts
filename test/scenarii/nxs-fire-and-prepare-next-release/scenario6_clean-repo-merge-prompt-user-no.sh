13bf905ff1458dc11b41259f239a5b882a2a89db
fnikitin <frederic.nikitin@gmail.com>
2015-10-08 18:31:42 +0200

    Add scenario 6 to test

---
+++ test/scenarii/nxs-fire-and-prepare-next-release/scenario6_clean-repo-merge-prompt-user-no.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd ${REPO_WO_DEP_PATH}
+
+printf 'n\n' | bash ${NX_SCRIPTS_PATH}/src/nxs-fire-and-prepare-next-release.sh ${NEXT_REPO_WO_DEP_VERSION_DEVELOP}
+
+if [ $? != 4 ]; then
+    TEST_RESULT+="failed"
+    FAILED_TESTS+=($0)
+else
+    TEST_RESULT+="passed"
+fi
+
+echo "reverting to clean git repo..."
+git merge --abort
