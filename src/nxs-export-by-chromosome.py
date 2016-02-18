9d8c56190fa23a1ec11c06ef110848382f2f87af
fnikitin <frederic.nikitin@gmail.com>
2016-02-18 11:42:31 +0100

    Add new option --thread to define the number of threads running the export

--- src/nxs-export-by-chromosome.py
+++ src/nxs-export-by-chromosome.py
@@ -2,11 +2,11 @@
 from Queue import Queue
 from threading import Thread
 import urllib2, time, argparse
+import multiprocessing
 
 url_path = "/export/entries"
-
-number_of_chromosome = 23
-num_threads = 8
+max_thread = multiprocessing.cpu_count()
+default_threads = multiprocessing.cpu_count()/2
 
 class Worker(Thread):
     """Thread executing tasks from a given tasks queue"""
@@ -43,41 +43,48 @@ class Timer(object):
     def duration_in_seconds(self):
         return self.__finish - self.__start
 
+def export(url, filename):
+    tim = Timer()
+    with tim:
+        print "fetching " + url + "\n"
+        file(filename, "w").write(urllib2.urlopen(url).read())
+    print url + " saved in " + filename + " done in " + str(tim.duration_in_seconds()) + "\n"
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Export neXtProt entries')
     parser.add_argument('api', help='the api url example: build-api.nextprot.org')
-    parser.add_argument('format', help='the output format, example: ttl or xml')
-    parser.add_argument('directory', help='the directory where to save the ouput files. example /work/ttldata/chromosome-new')
+    parser.add_argument('format', choices={'ttl', 'xml'}, help='the export format')
+    parser.add_argument('output', help='the directory where to save the output files. example /work/ttldata/chromosome-new')
+    parser.add_argument('-t', '--thread', metavar='num', default=default_threads, type=int, help='the number of threads (default='+ str(default_threads) + ')')
+
     args = parser.parse_args()
-   
-    def export(url, filename):
-        tim = Timer()
-        with tim:
-            print "fetching " + url + "\n"
-            file(filename, "w").write(urllib2.urlopen(url).read())
-        print url + " saved in " + filename + " done in " + str(tim.duration_in_seconds()) + "\n"
- 
-    pool = ThreadPool(num_threads)
+
+    if args.thread > max_thread:
+        parser.error("cannot run "+str(args.thread)+" threads (max="+str(max_thread)+")")
+    elif args.thread <= 0:
+        parser.error(str(args.thread)+" should be a positive number of threads")
+
+    pool = ThreadPool(args.thread)
     
     globalTimer = Timer()
     with globalTimer:
-        url = 'http://' + args.api + url_path  + "." + args.format + "?chromosome=";
+        url = 'http://' + args.api + url_path  + "." + args.format + "?chromosome="
         for n in range (1, 23):
-            filepath = args.directory + "/" + str(n) + "." + args.format;
+            filepath = args.output + "/" + str(n) + "." + args.format
             pool.add_task(export, url + str(n) , filepath)
         #Adding other chromosomes   
-        pool.add_task(export, url + "MT" , args.directory + "/MT." + args.format)
-        pool.add_task(export, url + "Y" , args.directory + "/Y." + args.format)
-        pool.add_task(export, url + "X" , args.directory + "/X." + args.format)
-        pool.add_task(export, url + "unknown" , args.directory + "/unknown." + args.format)
+        pool.add_task(export, url + "MT" , args.output + "/MT." + args.format)
+        pool.add_task(export, url + "Y" , args.output + "/Y." + args.format)
+        pool.add_task(export, url + "X" , args.output + "/X." + args.format)
+        pool.add_task(export, url + "unknown" , args.output + "/unknown." + args.format)
 
         if args.format == 'ttl':
             #Adding publications, schema, terminology and experimental contexts
-            pool.add_task(export, 'http://' + args.api + "/rdf/schema.ttl" , args.directory + "/schema.ttl")
-            pool.add_task(export, 'http://' + args.api + "/rdf/experimentalcontext.ttl" , args.directory + "/experimentalcontext.ttl")
-            pool.add_task(export, 'http://' + args.api + "/rdf/terminology.ttl" , args.directory + "/terminology.ttl")
-            pool.add_task(export, 'http://' + args.api + "/rdf/publication.ttl" , args.directory + "/publication.ttl")
+            pool.add_task(export, 'http://' + args.api + "/rdf/schema.ttl" , args.output + "/schema.ttl")
+            pool.add_task(export, 'http://' + args.api + "/rdf/experimentalcontext.ttl" , args.output + "/experimentalcontext.ttl")
+            pool.add_task(export, 'http://' + args.api + "/rdf/terminology.ttl" , args.output + "/terminology.ttl")
+            pool.add_task(export, 'http://' + args.api + "/rdf/publication.ttl" , args.output + "/publication.ttl")
 
         pool.wait_completion()
     print "Process finished in " + str(globalTimer.duration_in_seconds()) + "\n"
