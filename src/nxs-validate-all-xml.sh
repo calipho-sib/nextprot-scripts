a44a22fd0aa8d9b37660afb092c850df27e51047
fnikitin <frederic.nikitin@gmail.com>
2016-09-01 15:54:26 +0200

    Finalize script

--- src/nxs-validate-all-xml.sh
+++ src/nxs-validate-all-xml.sh
@@ -4,11 +4,11 @@ set -o pipefail # prevents errors in a pipeline from being masked. If any comman
 set -o nounset  # exit when your script tries to use undeclared variables.
 
 function echoUsage() {
-    echo "usage: $0 <xsd> <dir>" >&2
-    echo "This script validates all xml files found in path dir"
+    echo "usage: $0 <xsd> <xml-dir>" >&2
+    echo "This script validates xml files found in given directory and produces an xmllint output file"
     echo "Params:"
     echo " <xsd> xml schema of a nextprot entry"
-    echo " <dir> directory where xml are located"
+    echo " <xml-dir> directory where xml files to validate are located"
     echo "Options:"
     echo " -h print usage"
 }
@@ -36,17 +36,23 @@ fi
 
 xsd=$1
 dir=$2
-validation_file="nxs-validate-all-xml"_with_$(basename ${xsd})
+validation_file="xmllint"_with_$(basename ${xsd})
 
-echo "-- push directory ${dir}"
-pushd ${dir}
-entries=`find . -type f \( -iname "*.xml" ! -iname "nextprot_all.xml" \)`
+pushd ${dir} > /dev/null
+echo "-- searching xml files in directory '${dir}' ..."
+entries=`find . -type f \( -iname "*.xml" \)`
 
-echo -n "validation with xmllint..."
-xmllint --noout --schema ${xsd} ${entries} 2> ${validation_file}.log
-echo " Done"
+echo -n "-- running xmllint on files "${entries}...
+xmllint --noout --timing --stream --schema ${xsd} ${entries} 2> ${validation_file}.out
+echo " done"
 
-grep -v validate ${validation_file}.log | grep ':' | cut -d: -f3-6|sort|uniq > ${validation_file}.err
+grep -v validate ${validation_file}.out | grep ':' | cut -d: -f3-6|sort|uniq > ${validation_file}.err
 
-echo "-- pop directory"
-popd
+if [ -s ${validation_file}.err ]; then
+  echo "[validation failed]: error in file '${validation_file}.err'"  >&2
+  exit 2
+else
+  echo "[validation succeed]: output in file '${validation_file}.out'"
+fi
+
+popd > /dev/null
