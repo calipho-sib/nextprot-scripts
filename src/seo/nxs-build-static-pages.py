a0b3514d740e3502538f601865599effdf517b12
Daniel <ddtxra@gmail.com>
2016-12-22 15:16:43 +0100

    adding decorator

--- src/seo/nxs-build-static-pages.py
+++ src/seo/nxs-build-static-pages.py
@@ -1,36 +1,56 @@
-import urllib
+import socket
 import os
 from xml.dom import minidom
+import urllib2
+from decorators import retry
 
 import xml.etree.ElementTree as ET
 
 # Important to add the / at the end
 siteBase = "https://www.nextprot.org/"
-url = "https://api.nextprot.org/seo/sitemap.xml";
+sitemapUrl = siteBase + "sitemap.xml"
 
-xml_str = urllib.urlopen(url).read()
-xmldoc = minidom.parseString(xml_str)
+def saveToFile (content, filename):
+    text_file = open(filename, "w")
+    text_file.write(content)
+    text_file.close()
+    print str(cnt) + " creating file " + filename + " " 
 
-# Gets urls
-xmlUrls = xmldoc.getElementsByTagName('loc')
-
-# Gets a list of urls
-urls = [url.firstChild.nodeValue for url in xmlUrls]
-
-cnt = 0
-
-for url in urls:
-    cnt += 1
-    htmlfile = urllib.URLopener()
-    filename = url.replace(siteBase, "")
+def createDirectoryStructureIfNeeded(url, filename):
     if(filename):
         directoryname = os.path.dirname(filename)
-        if not os.path.exists(directoryname):
-            os.makedirs(directoryname)
-        print str(cnt) + " creating file " + filename + " " 
-        htmlfile.retrieve(url + "?_escaped_fragment_=", filename)
-    else:
-        print str(cnt) + " creating index.html" 
-        htmlfile.retrieve(url + "?_escaped_fragment_=", "index.html")
+        if(directoryname):
+            if not os.path.exists(directoryname):
+                os.makedirs(directoryname)
 
-        
+def getSitmapUrls():
+    xml_str = getUrlAsContent(sitemapUrl)
+    xmldoc = minidom.parseString(xml_str)
+
+    # Gets urls
+    xmlUrls = xmldoc.getElementsByTagName('loc')
+
+    # Gets a list of urls
+    return [url.firstChild.nodeValue for url in xmlUrls]
+
+
+@retry(urllib2.URLError, tries=4, delay=3, backoff=2)
+def getUrlAsContent(url):
+    print "asking for " + url
+    return urllib2.urlopen(url + "?_escaped_fragment_=", timeout=60).read()
+
+
+#Where to save stie
+dirlocation = "static-site/"
+
+cnt = 0
+# For each url in the sitemap
+for url in getSitmapUrls():
+    cnt += 1
+    filename = url.replace(siteBase, "")
+    createDirectoryStructureIfNeeded(url, dirlocation + filename)
+    content = getUrlAsContent(url)
+    if(filename):
+        saveToFile(content, dirlocation + filename)
+    else: 
+        saveToFile(content, dirlocation + "index.html")
