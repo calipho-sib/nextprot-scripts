89f4bc33c553d761e57c3b9392373e96e10ffd8c
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2018-01-31 17:33:25 +0100

    working on task ac-list

--- src/ttldata/quick-gen.sh
+++ src/ttldata/quick-gen.sh
@@ -27,7 +27,7 @@ function acLists() {
 
   logfile="generate-ac-lists-$(date "+%Y%m%d-%H%M").log"
 
-  url="${apibase}/entry-accessions.json"
+  url="${apibase}/entry-accessions.txt"
   outfile=/work/ttldata/ac_lists/nextprot_ac_list_all.txt
   wget --timeout=7200 --output-document=$outfile "$url" >> $logfile 2>&1
 
