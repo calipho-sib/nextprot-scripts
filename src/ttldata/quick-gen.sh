504d5b1d57342daf46fe29cae4a7338fccc41e08
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2018-01-31 16:11:56 +0100

    working on task ac-list

--- src/ttldata/quick-gen.sh
+++ src/ttldata/quick-gen.sh
@@ -30,6 +30,20 @@ function acLists() {
     outfile=/work/ttldata/ac_lists/nextprot_ac_list_chromosome_${chrname}.txt
     wget --timeout=7200 --output-document=$outfile "$url" >> $logfile 2>&1
   done
+  declare -a params=("PROTEIN_LEVEL" "TRANSCRIPT_LEVEL" "HOMOLOGY" "PREDICTED" "UNCERTAIN")
+  declare -a names=("PE1_at_protein_level" "PE2_at_transcript_level" "PE3_homology" "PE4_predicted" "PE5_uncertain")
+  lng=${#params[@]}
+  for (( i=0; i<${lng}; i++ )); do
+    echo $i " / " ${lng} 
+    param=${params[$i]}
+    url="${apibase}/entry-accessions/protein-existence/${param}.txt"
+    name=${names[$i]}
+    outfile=/work/ttldata/ac_lists/nextprot_ac_list_${name}.txt
+    echo name : $name
+    wcho param: $param
+    wget --timeout=7200 --output-document=$outfile "$url" >> $logfile 2>&1
+  done
+
 }
 
 function chrReports() {
