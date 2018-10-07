824cb29450df7e75b7b913aa2a26e041a5132932
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2018-10-07 15:22:58 +0200

    removed wrong chromosome name "0"

--- src/nxs-validate-all-xml.sh
+++ src/nxs-validate-all-xml.sh
@@ -36,7 +36,7 @@ fi
 
 xsd=$1
 dir=$2
-infiles="1 2 3 4 5 6 7 8 9 0 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y MT unknown nextprot_all"
+infiles="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y MT unknown nextprot_all"
 #infiles="Y MT"
 
 global_result=0
