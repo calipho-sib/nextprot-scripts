9dfbe44e92e356d6c8948a7aa6f52ff6c4a9d656
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2020-04-03 19:36:48 +0200

    new version of script checking virtuoso service

--- src/ttldata/check-virtuoso.sh
+++ src/ttldata/check-virtuoso.sh
@@ -1,15 +1,18 @@
 sleep_time=300
 response_timeout=10
-recipients="pierre-andre.michel@sib.swiss,Kasun.Samarasinghe@sib.swiss"
 
-QUERY="select count(*) where {?s ?p ?o}"
+recipients="pierre-andre.michel@sib.swiss,Kasun.Samarasinghe@sib.swiss,lydie.lane@sib.swiss,Alain.Gateau@sib.swiss"
+#recipients="pierre-andre.michel@sib.swiss"
+
+#QUERY="select count(*) where {?s ?p ?o}"
+QUERY="select * where {?s ?p ?o} limit 1"
 previous_state="UNKNOWN"
 
 body=""                
 body="$body \nHi,\n\nIn case of problem, use the following link"
 body="$body \nhttp://miniwatt:8900/view/operations/job/restart-virtuoso"
 body="$body \nto restart the service if you think it is necessary."
-body="$body \n\nCheers,\n/work/ttldata/check-virtuoso.sh"
+body="$body \n\nCheers,\n/work/ttldata/check-virtuoso.sh\n\n- - - - - -"
 
 while :
 do
@@ -21,6 +24,8 @@ do
 	if [ $previous_state != $state ]; then
 		subject="$(hostname) - virtuoso service status has changed from $previous_state to $state"
 		echo "$(date) - sending mail to tell status is now $state"
+		isql_status=$(isql 1111 dba dba exec='status()')
+		body="$body \n\n $isql_status"
 		echo -e "$body" | mail -s "$subject" "$recipients"
 	fi
 	previous_state=$state
