29f8beda2cb128dc61f5f29f00cf6519570fc0e0
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2021-04-16 10:28:21 +0200

    adapted script to handle column orcid

--- src/adapt-predictions.py
+++ src/adapt-predictions.py
@@ -4,7 +4,7 @@ f_in=open('fn_predictions.tsv')
 sep = '\t'
 headers=f_in.readline().strip().split(sep)
 errors=''
-if len(headers) < 7      : errors+='ERROR: missing columns in header at first line\n'
+if len(headers) < 8      : errors+='ERROR: missing columns in header at first line\n'
 if not headers[0] == 'AC': errors+='ERROR: column 1 should contain AC but found ' + headers[0]
 if not headers[1] == 'Gene name': errors+='ERROR: column 2 should contain Gene name but found ' + headers[1]
 if not headers[2] == 'GO term': errors+='ERROR: column 3 should contain GO term but found ' + headers[2]
@@ -12,30 +12,33 @@ if not headers[3] == 'GO identifier': errors+='ERROR: column 4 should contain GO
 if not headers[4] == 'ECO term': errors+='ERROR: column 5 should contain ECO term but found ' + headers[4]
 if not headers[5] == 'ECO AC': errors+='ERROR: column 6 should contain ECO AC but found ' + headers[5]
 if not headers[6] == 'PMID': errors+='ERROR: column 7 should contain PMID but found ' + headers[6]
+if not headers[7] == 'ORCID': errors+='ERROR: column 7 should contain ORCID but found ' + headers[7]
 if len(errors)>0 :
   print(errors)
   sys.exit()
 
 f_out = open('fn_predictions.ok', 'w')
+line_no=0
 while True:
   line = f_in.readline()
   if line == '': break
+  line_no += 1
   fields = line.strip().split(sep)
-  entry_ac   = fields[0]
-  cv_term_ac = fields[3]
-  evidence_code_ac = fields[5]
-  publication_ac = fields[6]
+  entry_ac   = fields[0].strip()
+  cv_term_ac = fields[3].strip()
+  evidence_code_ac = fields[5].strip()
+  publication_ac = fields[6].strip()
+  if publication_ac == '-': publication_ac = ''
   publication_db = 'PubMed'
-  if publication_ac == '':
-    publication_ac = None
-    publication_db = None
-  user_orcid = '0000-0002-0819-0473'
-  user_email = 'Paula.Duek@sib.swiss'
-  user_label = 'neXtProt'
-  user_hidden = True
+  if publication_ac == '': publication_db = ''
+  user_orcids = fields[7].strip().replace(' ','').replace('"','')
+  #print('line %d : orcid count %d' % (line_no, len( user_orcids.split(',')) ))
+  user_email = '' # ':Paula.Duek@sib.swiss'
+  user_label = '' # 'neXtProt'
+  user_hidden = False
   newline = ''
   newline += entry_ac + sep + cv_term_ac + sep + evidence_code_ac + sep + publication_ac + sep
-  newline += publication_db + sep + user_orcid + sep + user_email + sep + user_label + sep + str(user_hidden) + '\n' 
+  newline += publication_db + sep + user_orcids + sep + user_email + sep + user_label + sep + str(user_hidden) + '\n' 
   f_out.write(newline)
 
 f_in.close()
