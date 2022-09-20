33a35d353d26025555ad9a99e956c72a24c98337
Pierre-André <pierre-andre.michel@isb-sib.ch>
2022-09-20 10:58:13 +0200

    small changes related to image name

--- src/polish-rdf/insert-nx-model.sh
+++ src/polish-rdf/insert-nx-model.sh
@@ -1,2 +1,2 @@
 sed 's/<div class="figure">/<div class="figure" style="display:none;">/g' schema.html > schema.html.1
-sed 's/<h2>Overview<\/h2>/<img src="nx-model.png" style="max-width:100%;height:auto;" \/>/g' schema.html.1 > schema.html
+sed 's/<h2>Overview<\/h2>/<img src="nx-model-v2.png" style="max-width:100%;height:auto;" \/>/g' schema.html.1 > schema.html
--- src/polish-rdf/make-alpha.sh
+++ src/polish-rdf/make-alpha.sh
@@ -1,2 +1,3 @@
 sed 's/nextprot.org/alpha-nextprot.org/g' schema.ttl > alpha-schema.ttl
-python3 pylode/pyLODE-2.13.2/pylode/cli.py -i ./alpha-schema.ttl  > alpha-schema.html
+sed 's/nextprot.org/alpha-nextprot.org/g' schema.html > alpha-schema.html
+#python3 pylode/pyLODE-2.13.2/pylode/cli.py -i ./alpha-schema.ttl  > alpha-schema.html
